435adad4f5aac5882f2e509435c7fbb67a23b9c2
DianaC18 <dc.cepeda@uniandes.edu.co>
2018-04-25 12:14:04 -0500

    Conexion

--- IVote/src/dao/DAOCandidato.java
+++ IVote/src/dao/DAOCandidato.java
@@ -1,6 +1,75 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+
+import vos.VOCandidato;
 
 public class DAOCandidato {
+	/**
+	 * Arraylits de recursos que se usan para la ejecución de sentencias SQL
+	 */
+	private ArrayList<Object> recursos;
 
+	/**
+	 * Atributo que genera la conexión a la base de datos
+	 */
+	private Connection conn;
+
+	/**
+	 * Metodo constructor que crea DAOVideo
+	 * <b>post: </b> Crea la instancia del DAO e inicializa el Arraylist de recursos
+	 */
+	public DAOCandidato() {
+		recursos = new ArrayList<Object>();
+	}
+
+	/**
+	 * Metodo que cierra todos los recursos que estan enel arreglo de recursos
+	 * <b>post: </b> Todos los recurso del arreglo de recursos han sido cerrados
+	 */
+	public void cerrarRecursos() {
+		for(Object ob : recursos){
+			if(ob instanceof PreparedStatement)
+				try {
+					((PreparedStatement) ob).close();
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+		}
+	}
+
+	/**
+	 * Metodo que inicializa la connection del DAO a la base de datos con la conexión que entra como parametro.
+	 * @param con  - connection a la base de datos
+	 */
+	public void setConn(Connection con){
+		this.conn = con;
+	}
+	
+	
+	public ArrayList<VOCandidato> darCandidatos() throws SQLException, Exception {
+		ArrayList<VOCandidato> candidatos = new ArrayList<VOCandidato>();
+
+		String sql = "SELECT * FROM sistema.candidatos";
+
+		PreparedStatement prepStmt = conn.prepareStatement(sql);
+		recursos.add(prepStmt);
+		ResultSet rs = prepStmt.executeQuery();
+
+		while (rs.next()) {
+			Long id = rs.getLong("ID");
+			String nombre = rs.getString("NOMBRE");
+			String genero = rs.getString("GENERO");
+			Date fechaNacimiento = rs.getDate("FECHA_NACIMIENTO");
+			candidatos.add(new VOCandidato(id, nombre, genero, fechaNacimiento));
+		}
+		return candidatos;
+	}
+	
 }
